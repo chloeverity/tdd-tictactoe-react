73248ec1a6ff3732e6bc5463808d76e73af2b4fd
Chloe Verity <chloeverity@hotmail.co.uk>
2019-01-30 12:22:59 +0000

    minor refactor in test

--- tictactoe/cypress/integration/viewboard.spec.js
+++ tictactoe/cypress/integration/viewboard.spec.js
@@ -1,10 +1,8 @@
-describe('Next player:', () => {
+describe('Viewing the board', () => {
   it('Displays who the next player is', () => {
     cy.visit('http://localhost:3000')
     cy.contains("Next player: X")
   });
-});
-describe('Viewing the board', () => {
   it('Displays the grid', () => {
     cy.visit('http://localhost:3000')
     cy.get('[class="square"]')
