6ce6f4fae328f558b85754fb6c11a022854d6d96
Chloe Verity <chloeverity@hotmail.co.uk>
2019-01-30 12:16:43 +0000

    writing another passing test for grid contents

--- tictactoe/cypress/integration/viewboard.spec.js
+++ tictactoe/cypress/integration/viewboard.spec.js
@@ -1,7 +1,13 @@
-
-describe('Viewing the board', () => {
+describe('Next player:', () => {
   it('Displays who the next player is', () => {
     cy.visit('http://localhost:3000')
     cy.contains("Next player: X")
-  })
-})
+  });
+});
+describe('Viewing the board', () => {
+  it('Displays the grid', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('[class="square"]')
+      .contains('0')
+  });
+});
